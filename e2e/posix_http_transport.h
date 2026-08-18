fe46a4098c3426dc183a8fa8b7e1db9455aa9404
Roman Popov <atfpom4h@gmail.com>
2026-08-18 19:02:22 +0300

    add POSIX HTTP transport interface for E2E

---
+++ e2e/posix_http_transport.h
@@ -0,0 +1,25 @@
+#ifndef max_posix_http_transportH
+#define max_posix_http_transportH
+
+#include "../maxclient.h"
+
+class TPosixHttpTransport : public IMaxHttpTransport
+{
+public:
+    virtual MAX_HTTP_RESPONSE Get(const std::string & url,
+        const std::map<std::string,std::string> & headers);
+    virtual MAX_HTTP_RESPONSE Post(const std::string & url,
+        const std::map<std::string,std::string> & headers,
+        const std::string & body);
+    virtual MAX_HTTP_RESPONSE PostMultipartFile(const std::string & url,
+        const std::map<std::string,std::string> & headers,
+        const std::string & fieldName,
+        const std::string & filename);
+private:
+    MAX_HTTP_RESPONSE Request(const std::string & method,
+        const std::string & url,
+        const std::map<std::string,std::string> & headers,
+        const std::string & body);
+};
+
+#endif
