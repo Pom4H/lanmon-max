3538dc42e5f8ac734025520c81f809ba8d3945c4
Roman Popov <atfpom4h@gmail.com>
2026-08-19 12:38:48 +0300

    document MAX BotApi form implementation

--- Max/UFMaxBotApi.cpp
+++ Max/UFMaxBotApi.cpp
@@ -7,8 +7,10 @@
 #pragma resource "*.dfm"
 TFormMaxBotApi *FormMaxBotApi;
 //---------------------------------------------------------------------------
+//Конструктор
 __fastcall TFormMaxBotApi::TFormMaxBotApi(TComponent* Owner):TForm(Owner){}
 //---------------------------------------------------------------------------
+//Данные MAX бота
 void __fastcall TFormMaxBotApi::FormCreate(TObject *Sender)
 {
     OldBotApi=MaxBot.BotApi;
@@ -17,16 +19,24 @@ void __fastcall TFormMaxBotApi::FormCreate(TObject *Sender)
     EditFirstName->Text=MaxBot.MyBotInfo.first_name;
     EditUsername->Text=MaxBot.MyBotInfo.username;
     ButtonTestBotApi->Enabled=EditBotApi->Text.Length()!=0;
+    //OK разрешается только после успешного GetMe
     ButtonOk->Enabled=false;
 }
 //---------------------------------------------------------------------------
+//Изменён BotApi/token
 void __fastcall TFormMaxBotApi::EditBotApiChange(TObject *Sender)
-{ButtonTestBotApi->Enabled=EditBotApi->Text.Length()!=0;ButtonOk->Enabled=false;}
+{
+    ButtonTestBotApi->Enabled=EditBotApi->Text.Length()!=0;
+    //После изменения токена предыдущий результат проверки больше не действителен
+    ButtonOk->Enabled=false;
+}
 //---------------------------------------------------------------------------
+//Проверить BotApi запросом GetMe
 void __fastcall TFormMaxBotApi::ButtonTestBotApiClick(TObject *Sender)
 {
     NewMyBotInfo.Clear();
     NewBotApi=EditBotApi->Text;
+    //Временно передать новый token в MAX_BOT и выполнить GetMe через рабочий поток
     MaxBot.BotApi=NewBotApi;
     MaxBot.GetMe();
 }
@@ -34,7 +44,13 @@ void __fastcall TFormMaxBotApi::ButtonTestBotApiClick(TObject *Sender)
 //Получен ответ GetMe
 void __fastcall TFormMaxBotApi::OnGetMe(MaxBotInfo &botinfo)
 {
-    if(!botinfo.Valid){ButtonOk->Enabled=false;return;}
+    if(!botinfo.Valid)
+    {
+        //MAX не вернул валидные данные бота
+        ButtonOk->Enabled=false;
+        return;
+    }
+    //Данные MAX бота
     NewMyBotInfo=botinfo;
     EditId->Text=botinfo.Id;
     EditFirstName->Text=botinfo.first_name;
