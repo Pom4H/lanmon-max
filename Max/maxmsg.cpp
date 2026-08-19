1a6a82821d9db1922af03b6d3085564310e83319
Roman Popov <atfpom4h@gmail.com>
2026-08-19 12:36:09 +0300

    restore Telegram-style comments in MAX messages

--- Max/maxmsg.cpp
+++ Max/maxmsg.cpp
@@ -9,6 +9,7 @@
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 //---------------------------------------------------------------------------
+//Преобразование входящего UTF-8 MAX в AnsiString/CP1251
 AnsiString MaxAnsiFromUtf8(const std::string & s)
 {
     if(s.empty())return "";
@@ -23,6 +24,7 @@ AnsiString MaxAnsiFromUtf8(const std::string & s)
     return AnsiString(&a[0]);
 }
 //---------------------------------------------------------------------------
+//Копирование данных отправителя сообщения
 void MaxFrom::CopyFrom(MaxFrom * from)
 {
     Id=from->Id;
@@ -32,6 +34,7 @@ void MaxFrom::CopyFrom(MaxFrom * from)
     language_code=from->language_code;
 }
 //---------------------------------------------------------------------------
+//Полное имя отправителя
 AnsiString MaxFrom::GetFullName(void)
 {
     AnsiString s;
@@ -44,8 +47,9 @@ AnsiString MaxFrom::GetFullName(void)
     return s;
 }
 //---------------------------------------------------------------------------
+//Объект Chat
 //---------------------------------------------------------------------------
-//---------------------------------------------------------------------------
+//Копирование данных чата
 void MaxChat::CopyFrom(MaxChat * chat)
 {
     Id=chat->Id;
@@ -55,6 +59,7 @@ void MaxChat::CopyFrom(MaxChat * chat)
     username=chat->username;
 }
 //---------------------------------------------------------------------------
+//Полное имя чата/собеседника
 AnsiString MaxChat::GetFullName(void)
 {
     if(username.Length())return username;
@@ -68,6 +73,9 @@ AnsiString MaxChat::GetFullName(void)
     return s;
 }
 //---------------------------------------------------------------------------
+//Сообщение MAX
+//---------------------------------------------------------------------------
+//Представление сообщения одной строкой для отладки
 AnsiString MaxMessage::AsString(void)
 {
     AnsiString s;
@@ -78,6 +86,7 @@ AnsiString MaxMessage::AsString(void)
     return s;
 }
 //---------------------------------------------------------------------------
+//Текст даты времени сообщения
 AnsiString MaxMessage::GetDateText(void)
 {
     time_t t=(time_t)Date;
@@ -90,6 +99,7 @@ AnsiString MaxMessage::GetDateText(void)
     return s;
 }
 //---------------------------------------------------------------------------
+//Дата и время сообщения
 TDateTime MaxMessage::GetDateTime(void)
 {
     time_t t=(time_t)Date;
@@ -100,6 +110,7 @@ TDateTime MaxMessage::GetDateTime(void)
     return dt;
 }
 //---------------------------------------------------------------------------
+//Текст типа сообщения
 AnsiString MaxMessage::GetTypeText(void)
 {
     switch(Type)
@@ -124,6 +135,7 @@ AnsiString MaxMessage::GetParticipantText(void)
     return "";
 }
 //---------------------------------------------------------------------------
+//Копирование сообщения
 void MaxMessage::CopyFrom(MaxMessage * msg)
 {
     Type=msg->Type;
@@ -136,60 +148,73 @@ void MaxMessage::CopyFrom(MaxMessage * msg)
     Text=msg->Text;
 }
 //---------------------------------------------------------------------------
+//Получение сообщения из нормализованного ответа MAX API
 void MaxMessage::CopyFrom(const MAX_MESSAGE & msg)
 {
     Type=mmtMESSAGE;
     update_id=(__int64)msg.UpdateTimestamp;
     message_id=msg.MessageId.c_str();
+    //Объект from
     From.Clear();
     From.Id=MaxInt64ToString(msg.UserId).c_str();
     From.is_bot=msg.SenderIsBot;
     From.first_name=MaxAnsiFromUtf8(msg.FirstName);
     From.last_name=MaxAnsiFromUtf8(msg.LastName);
+    //Объект chat
     Chat.Clear();
     Chat.Id=MaxInt64ToString(msg.ChatId).c_str();
     Chat.type=msg.ChatType.c_str();
     Chat.first_name=From.first_name;
     Chat.last_name=From.last_name;
     Chat.username=MaxAnsiFromUtf8(msg.UserName);
+    //Дата и текст сообщения
     Date=(long)(msg.MessageTimestamp/1000);
     Text=MaxAnsiFromUtf8(msg.Text);
 }
 //---------------------------------------------------------------------------
 //Список сообщений
 //---------------------------------------------------------------------------
+//Конструктор
 MaxMessage_LIST::MaxMessage_LIST()
 {
+    //Список
     List=new TList;
 }
 //---------------------------------------------------------------------------
+//Деструктор
 MaxMessage_LIST::~MaxMessage_LIST()
 {
     Clear();
     delete List;
 }
 //---------------------------------------------------------------------------
+//Доступ по индексу к сообщениям
 MaxMessage * MaxMessage_LIST::Get(int index)
 {
     if(index<0 || index>=Count)return NULL;
     return (MaxMessage *)List->Items[index];
 }
 //---------------------------------------------------------------------------
+//Удалить сообщение по индексу
 void MaxMessage_LIST::Delete(int index)
 {
     MaxMessage * tm=Get(index);
     if(tm)
     {
+        //Удалить объект сообщения
         delete tm;
+        //Удалить указатель в списке
         List->Delete(index);
     }
 }
 //---------------------------------------------------------------------------
+//Удалить все сообщения
 void MaxMessage_LIST::Clear(void)
 {
     while(Count)Delete(0);
 }
 //---------------------------------------------------------------------------
+//Удалить сообщение
 void MaxMessage_LIST::Remove(MaxMessage * tm)
 {
     for(int i=0;i<Count;i++)
@@ -199,6 +224,7 @@ void MaxMessage_LIST::Remove(MaxMessage * tm)
     }
 }
 //---------------------------------------------------------------------------
+//Поиск сообщения
 MaxMessage * MaxMessage_LIST::Find(__int64 update_id)
 {
     for(int i=0;i<Count;i++)
@@ -209,6 +235,7 @@ MaxMessage * MaxMessage_LIST::Find(__int64 update_id)
     return NULL;
 }
 //---------------------------------------------------------------------------
+//Добавление сообщения
 MaxMessage * MaxMessage_LIST::Add(void)
 {
     MaxMessage * tm=new MaxMessage();
@@ -216,8 +243,10 @@ MaxMessage * MaxMessage_LIST::Add(void)
     return tm;
 }
 //---------------------------------------------------------------------------
+//Копирование
 void MaxMessage_LIST::CopyFrom(MaxMessage_LIST & msglist)
 {
+    //Удалить все сообщения
     Clear();
     for(int i=0;i<msglist.Count;i++)
     {
@@ -226,12 +255,15 @@ void MaxMessage_LIST::CopyFrom(MaxMessage_LIST & msglist)
     }
 }
 //---------------------------------------------------------------------------
+//Получение сообщений из ответа MAX API
 void MaxMessage_LIST::CopyFrom(const MAX_UPDATES & updates)
 {
     Clear();
+    //MAX_API_CLIENT уже разобрал JSON; здесь сохраняем Telegram-подобный список сообщений
     for(size_t i=0;i<updates.Messages.size();i++)Add()->CopyFrom(updates.Messages[i]);
 }
 //---------------------------------------------------------------------------
+//Получить последнее значение update_id
 __int64 MaxMessage_LIST::GetUpdateId(void)
 {
     __int64 result=0;
@@ -250,6 +282,7 @@ void MaxUser::Save(TFastIniFile * ini,AnsiString section)
     if(FOutCount)ini->WriteString(section,"OutCount",IntToStr((__int64)FOutCount));
     if(FComment.Length())ini->WriteString(section,"Comment",FComment);
     if(FTag)ini->WriteInteger(section,"Tag",FTag);
+    //MAX-специфичное поле: способ адресации user_id/chat_id
     if(FPeerType==maxPeerChat)ini->WriteString(section,"PeerType","chat");
 }
 //---------------------------------------------------------------------------
@@ -265,9 +298,11 @@ void MaxUser::Load(TFastIniFile * ini,AnsiString section)
     FOutCount=_atoi64(ini->ReadString(section,"OutCount","0").c_str());
     FComment=ini->ReadString(section,"Comment","");
     FTag=ini->ReadInteger(section,"Tag",0);
+    //Для старых конфигураций без PeerType используется личный пользователь
     FPeerType=ini->ReadString(section,"PeerType","user").LowerCase()=="chat"?maxPeerChat:maxPeerUser;
 }
 //---------------------------------------------------------------------------
+//Копирование
 void MaxUser::CopyFrom(MaxUser * user)
 {
     FId=user->FId;
@@ -281,6 +316,7 @@ void MaxUser::CopyFrom(MaxUser * user)
     FPeerType=user->FPeerType;
 }
 //---------------------------------------------------------------------------
+//Заполнить пользователя из отправителя сообщения
 void MaxUser::CopyUserFrom(MaxMessage * msg)
 {
     Clear();
@@ -290,6 +326,7 @@ void MaxUser::CopyUserFrom(MaxMessage * msg)
     FPeerType=maxPeerUser;
 }
 //---------------------------------------------------------------------------
+//Заполнить пользователя из чата сообщения
 void MaxUser::CopyChatFrom(MaxMessage * msg)
 {
     Clear();
@@ -299,50 +336,63 @@ void MaxUser::CopyChatFrom(MaxMessage * msg)
     FPeerType=maxPeerChat;
 }
 //---------------------------------------------------------------------------
+//Проверка, что пользователь соответствует маске псевдонима
 bool MaxUser::HasValidAlias(AnsiString alias)
 {
     if(!Valid)return false;
     if(alias=="*")return true;
     if(alias.IsEmpty())return true;
+    //Длина маски
     int len=alias.Length();
     if(FAlias.Length()<len)return false;
     for(int i=1;i<=len;i++)
     {
+        //Символ '!' означает любой символ в этой позиции
         if(alias[i]!='!' && FAlias[i]!=alias[i])return false;
     }
     return true;
 }
 //---------------------------------------------------------------------------
+//Список пользователей
+//---------------------------------------------------------------------------
+//Конструктор
 __fastcall MaxUser_LIST::MaxUser_LIST()
 {
 }
 //---------------------------------------------------------------------------
+//Деструктор
 __fastcall MaxUser_LIST::~MaxUser_LIST()
 {
     ClearList();
 }
 //---------------------------------------------------------------------------
+//Доступ по индексу к пользователям
 MaxUser * MaxUser_LIST::Get(int index)
 {
     if(index<0 || index>=Count)return NULL;
     return (MaxUser *)Items[index];
 }
 //---------------------------------------------------------------------------
+//Удалить пользователя по индексу
 void MaxUser_LIST::DeleteUser(int index)
 {
     MaxUser * user=Get(index);
     if(user)
     {
+        //Удалить объект пользователя
         delete user;
+        //Удалить указатель в списке
         TList::Delete(index);
     }
 }
 //---------------------------------------------------------------------------
+//Удалить всех пользователей
 void MaxUser_LIST::ClearList(void)
 {
     while(Count)DeleteUser(0);
 }
 //---------------------------------------------------------------------------
+//Удалить пользователя по указателю
 void MaxUser_LIST::Remove(MaxUser * user)
 {
     for(int i=0;i<Count;i++)
@@ -352,6 +402,7 @@ void MaxUser_LIST::Remove(MaxUser * user)
     }
 }
 //---------------------------------------------------------------------------
+//Поиск пользователя по идентификатору Id
 MaxUser * MaxUser_LIST::Find(AnsiString id)
 {
     for(int i=0;i<Count;i++)
@@ -362,11 +413,13 @@ MaxUser * MaxUser_LIST::Find(AnsiString id)
     return NULL;
 }
 //---------------------------------------------------------------------------
+//Поиск индекса идентификатора в списке
 int MaxUser_LIST::IndexOfId(AnsiString id)
 {
     return IndexOf(Find(id));
 }
 //---------------------------------------------------------------------------
+//Поиск псевдонима
 MaxUser * MaxUser_LIST::FindAlias(AnsiString alias)
 {
     for(int i=0;i<Count;i++)
@@ -377,6 +430,7 @@ MaxUser * MaxUser_LIST::FindAlias(AnsiString alias)
     return NULL;
 }
 //---------------------------------------------------------------------------
+//Добавление пользователя
 MaxUser * MaxUser_LIST::AddUser(void)
 {
     MaxUser * user=new MaxUser();
@@ -384,11 +438,13 @@ MaxUser * MaxUser_LIST::AddUser(void)
     return user;
 }
 //---------------------------------------------------------------------------
+//Сохранить всех пользователей
 void MaxUser_LIST::Save(TFastIniFile * ini)
 {
     for(int i=0;i<Count;i++)Get(i)->Save(ini,"User"+IntToStr(i));
 }
 //---------------------------------------------------------------------------
+//Загрузить всех пользователей
 void MaxUser_LIST::Load(TFastIniFile * ini)
 {
     ClearList();
@@ -402,6 +458,7 @@ void MaxUser_LIST::Load(TFastIniFile * ini)
     }
 }
 //---------------------------------------------------------------------------
+//Получить свободный псевдоним автоматически
 AnsiString MaxUser_LIST::GetFreeAlias(void)
 {
     int num=1;
@@ -410,30 +467,40 @@ AnsiString MaxUser_LIST::GetFreeAlias(void)
     return alias;
 }
 //---------------------------------------------------------------------------
+//Заполнить список пользователями, соответствующих маске псевдонима
 void MaxUser_LIST::GetUsersByAlias(MaxUser_LIST * userlist,AnsiString alias)
 {
     userlist->ClearList();
     for(int i=0;i<Count;i++)
     {
         MaxUser * ui=Get(i);
+        //Проверка, что пользователь соответствует маске псевдонима
         if(ui->HasValidAlias(alias))
         {
+            //Добавление пользователя
             MaxUser * user=userlist->AddUser();
             user->CopyFrom(ui);
         }
     }
 }
 //---------------------------------------------------------------------------
+//Заполнить список индексами пользователей, соответствующих маске псевдонима
 void MaxUser_LIST::GetIndexesByAlias(TList * list,AnsiString alias)
 {
     list->Clear();
     for(int i=0;i<Count;i++)
     {
         MaxUser * ui=Get(i);
-        if(ui->HasValidAlias(alias))list->Add((void *)i);
+        //Проверка, что пользователь соответствует маске псевдонима
+        if(ui->HasValidAlias(alias))
+        {
+            //Добавление индекса пользователя
+            list->Add((void *)i);
+        }
     }
 }
 //---------------------------------------------------------------------------
+//Получение информации о боте из ответа MAX API
 void MaxBotInfo::CopyFrom(const MAX_BOT_INFO & bi)
 {
     Clear();
@@ -442,6 +509,7 @@ void MaxBotInfo::CopyFrom(const MAX_BOT_INFO & bi)
     username=MaxAnsiFromUtf8(bi.UserName);
 }
 //---------------------------------------------------------------------------
+//Копирование информации о боте
 void MaxBotInfo::CopyFrom(MaxBotInfo & bi)
 {
     Id=bi.Id;
